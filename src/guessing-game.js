2e34faed9bf0c8e38a4005c8e9e2ef882553f7f6
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-09-23 13:14:51 +0300

    feat: make guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+      }
 
-    setRange(min, max) {
-
-    }
+      setRange(min, max) {
+        this.minRange = min;
+        this.maxRange = max;
+      }
 
     guess() {
-
+        const rangeSize = this.maxRange - this.minRange;
+        const midpoint = Math.round(this.minRange + rangeSize / 2);
+        return midpoint;
     }
 
     lower() {
-
+        this.maxRange = this.guess();
     }
 
     greater() {
-
+        this.minRange = this.guess();
     }
 }
 
